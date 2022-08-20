553a52c2d8ce43374c619c688c426cbb0e0b626c
alrbs10 <89731330+alrbs10@users.noreply.github.com>
2022-08-21 01:14:03 +0900

    reserv & application 4 mobile (witout refactoring...)

---
+++ src/components/Mobile/InfoTabSmall.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import DetailInfoBox from "../Reservation/DetailInfo";
+
+export default function InfoTabSmall({ data }) {
+  return (
+    <div
+      style={{
+        borderTop: "1px solid gray",
+        borderBottom: "1px solid gray",
+        margin: "0 15px",
+        padding: "15px 0",
+        display: "flex",
+      }}
+    >
+      <div style={{ width: "40%" }}>
+        <img
+          src="https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/product/1590136745/B.jpg?180000000"
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      </div>
+      <div style={{ marginLeft: "10px" }}>
+        <Title>어떤 테니스장</Title>
+        <DetailInfoBox data={data} />
+      </div>
+    </div>
+  );
+}
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+  padding-bottom: 3px;
+  border-bottom: 4px solid black;
+  width: max-content;
+  margin-bottom: 15px;
+`;
---
+++ src/components/Mobile/InputContainerBoxSmall.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function InputContainerBoxSmall({ onSubmit }) {
+  return (
+    <>
+      <InputContainer>
+        <Label>예약자 이름</Label>
+        <InputBox placeholder="이름" />
+      </InputContainer>
+      <InputContainer>
+        <Label>전화번호</Label>
+        <InputBox placeholder="010-XXXX-XXXX" />
+      </InputContainer>
+      <InputContainer>
+        <Label>동행 인원</Label>
+        <InputBox placeholder="00명" />
+      </InputContainer>
+      <InputContainer>
+        <Label>예약 시간</Label>
+        <InputBox placeholder="00시" />
+      </InputContainer>
+      <BtnContainer>
+        <Btn onClick={onSubmit} type="reserv">
+          예약하기
+        </Btn>
+        <CancelBtn>취소</CancelBtn>
+      </BtnContainer>
+    </>
+  );
+}
+const InputContainer = styled.div`
+  display: flex;
+  margin-bottom: 5px;
+`;
+const Label = styled.div`
+  width: 30%;
+  font-size: 14px;
+  font-weight: bolder;
+`;
+const InputBox = styled.input`
+  outline: none;
+  border: 2px solid #10dd3d;
+  padding-left: 10px;
+  font-size: 14px;
+  ::placeholder {
+    color: #10dd3d;
+  }
+`;
+const BtnContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+`;
+const Btn = styled.div`
+  width: 48%;
+  padding: 10px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #10dd3d;
+  color: white;
+  cursor: pointer;
+  font-size: 14px;
+`;
+const CancelBtn = styled(Btn)`
+  color: white;
+  background-color: gray;
+`;
